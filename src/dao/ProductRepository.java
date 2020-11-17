ffdd1c0834bdf087de1e72d5e60f4b3ea4d564e5
FIRSTuser <FIRSTuser@DESKTOP-596EGFK>
2020-11-17 18:06:20 +0900

    Shopping Mall Project 3rd Upload

---
+++ src/dao/ProductRepository.java
@@ -0,0 +1,57 @@
+package dao;
+
+import java.util.ArrayList;
+
+import dto.Product;
+
+public class ProductRepository {
+
+	private ArrayList<Product> listOfProducts = new ArrayList<Product>();
+	
+	public ProductRepository() {
+		
+		Product phone = new Product("p1234","iphon 12", 1000000);
+		phone.setDescription("6.1-inch, 2532X1170 Super Retina XDR display, µà¾ó 12MP Ä«¸Þ¶ó");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
+		
+		Product notebook = new Product("p1235","LG PC Gram", 1500000);
+		notebook.setDescription("13.3-inch, IPS LED display, Generation Intel Core processors");
+		notebook.setCategory("NoteBook");
+		notebook.setManufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refurbished");
+		
+		Product tablet = new Product("p1236","Galaxy Tab 5", 900000);
+		tablet.setDescription("212.8*125.6*6mm, Super AMOLED display, Octa-Core processors");
+		tablet.setCategory("Tablet");
+		tablet.setManufacturer("SAMSUNG");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
+		
+		listOfProducts.add(phone);
+		listOfProducts.add(notebook);
+		listOfProducts.add(tablet);
+	
+	}
+	
+	public ArrayList<Product> getAllProducts(){
+		return listOfProducts;
+	}
+	
+	public Product getProductById(String productId) {
+		Product productById = null;
+		
+		for(int i = 0; i<listOfProducts.size(); i++) {
+			Product product = listOfProducts.get(i);
+			if(product != null && product.getProductId() != null && product.getProductId().equals(productId)) {
+				productById = product;
+				break;
+			}
+		}
+		
+		return productById;
+	}
+}
